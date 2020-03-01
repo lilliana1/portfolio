3c8251d3a74b174465bf57574123f088a10ad1c7
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-03-01 17:19:08 -0500

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ function About(props) {
             </p>
 
             <p>
-            Born and raised in the beautiful Island of Puerto Rico. I currently live in Orlando, FL. I enjoy anything that has to do meeting people from around the world. Besides meeting people, I enjoy Spanish Cinema, traveling, roadtrips, exploring my island and trying new places to eat!
+            Born and raised in the beautiful Island of Puerto Rico. I currently live in Orlando, FL. I enjoy anything that has to do meeting people from around the world. Besides meeting people, I enjoy Spanish Cinema, traveling, roadtrips, exploring my island and trying new places to eat. 
             </p>
 
             
