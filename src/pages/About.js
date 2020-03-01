f41d54774159ff630f06feac96a249c4327352cd
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-03-01 10:51:42 -0500

    edit about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About(props) {
             <Head title={props.title} />
             <Content>
             <p>
-            Full Stack Web Developer graduate from the three-month program at the University of Central Florida. Prepared to utilize my skillset in web development areas, including HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB. Acquired a BBA in Computer Information Systems from the University of Puerto Rico. Also, worked as a technological assistant and data entry. As a technological assistant, I worked directly with the Doctor doing Electronic Patient Assessment. Due to my diligent nature, I am a team player, organized, responsible and easily adapt to different workplace scenarios.Created Foodie which utilized HTML, CSS, Bootstrap, animated.css and JS and won second place best project on a JS Coding Bootcamp in PR. Looking forward to bringing my wealth of experience to support operations.
+            Full Stack Web Developer graduate from the three-month program at the University of Central Florida. Prepared to utilize my skillset in web development areas, including HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB. Acquired a BBA in Computer Information Systems from the University of Puerto Rico. Due to my diligent nature, I am a team player, organized, responsible and easily adapt to different workplace scenarios. Created Foodie which utilized HTML, CSS, Bootstrap, animated.css and JS and won second place best project on a JS Coding Bootcamp in PR. Looking forward to bringing my wealth of experience to support operations.
             </p>
 
             <p>
